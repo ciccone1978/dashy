6349173a5daf6118d757e9ab81a95d1088e15962
ciccone1978 <ciccone1978@gmail.com>
2022-08-04 16:07:52 +0200

    Confirm password and terms on the registration form

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Email, DataRequired, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Email, DataRequired, EqualTo, ValidationError
+from apps.authentication.models import Users
 
 #login
 class LoginForm(FlaskForm):
@@ -10,10 +11,23 @@ class LoginForm(FlaskForm):
 
 #registration
 class CreateAccountForm(FlaskForm):
-    username = StringField('Username', id='username_create', validators=[DataRequired()])
-    email = StringField('Email', id='email_create', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', id='pwd_create', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
+    agreement = BooleanField('I agree to the terms', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
+    #custom validators
+    def validate_username(self, username):
+        user = Users.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already registered')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email already registered')
 
 #reset password request
 class ResetPasswordRequestForm(FlaskForm):
