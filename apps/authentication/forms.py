b8130cc01bfba37814578de1db37f64e0523fbfd
ciccone1978 <ciccone1978@gmail.com>
2022-08-05 12:15:20 +0200

    Validate password against a stronger policy

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -1,6 +1,7 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import Email, DataRequired, EqualTo, ValidationError
+from wtforms.validators import Email, DataRequired, EqualTo, ValidationError, Length
 from apps.authentication.models import Users
 
 #login
@@ -20,6 +21,10 @@ class CreateAccountForm(FlaskForm):
 
     #custom validators
     def validate_username(self, username):
+        username_len = 4
+        if len(username.data) < username_len:
+            raise ValidationError(f'Username should be at least {username_len} characters long')
+
         user = Users.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Username already registered')
@@ -28,6 +33,19 @@ class CreateAccountForm(FlaskForm):
         user = Users.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Email already registered')
+
+    def validate_password(self, password):
+        """
+        Password strenght policy
+        - at least 8 chars long {8,}
+        - at least 1 uppercase letter (?=.*?[A-Z])
+        - at lesst 1 lowercase letter (?=.*?[a-z])
+        - at least 1 digit (?=.*?[0-9])
+        - at least 1 special char (?=.*?[#?!@$%^&*-])
+        """
+        password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+        if re.match(password_pattern, password.data) is None:
+            raise ValidationError('Please use a stronger password')        
 
 #reset password request
 class ResetPasswordRequestForm(FlaskForm):
